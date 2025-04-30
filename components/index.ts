21746b2a492e9aae0eca095c6c6bc389f9d21907
thisura.munasinghe <thisurachesster@gmail.com>
2025-04-29 21:50:42 -0400

    Add-Filter

---
+++ components/index.ts
@@ -0,0 +1,15 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
